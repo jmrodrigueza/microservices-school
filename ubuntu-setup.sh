a4759a21bdcf09783bf76abb2d60ad7306ade690
jmrodrigueza <jmrodriguez@gmv.com>
2017-11-21 17:32:42 +0100

    Changes of today

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -4,10 +4,14 @@
 sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -v
+npm config set http-proxy http://ptmproxy.gmv.es
+npm config set http-proxy http://ptmproxy.gmv.es
 nvm install 7
-nvm use
-npm install –g yarn
+nvm use 7
+npm install -g yarn
 pushd recipes-api
+npm config set http http://ptmproxy.gmv.es
+npm config set http-proxy http://ptmproxy.gmv.es
 yarn install
 popd
 pushd recipes-crawler
